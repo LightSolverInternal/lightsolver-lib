1dec7ab291f89c1e94181415130c544aead149c1
AndreyKLS <andreyk@lightsolver.com>
2025-08-29 01:14:04 +0300

    Update __init__.py

--- ls_lib/__init__.py
+++ ls_lib/__init__.py
@@ -1,3 +1,12 @@
-from .ls_lib import print_hello
+from .ls_lib import calc_ising_energies
+from .ls_lib import probmat_qubo_to_ising
+from .ls_lib import probmat_ising_to_qubo
+from .ls_lib import create_random_initial_states
+from .ls_lib import calc_ising_energy_from_states
 
-__all__ = ['print_hello']
+__all__ = ['calc_ising_energies',
+           'probmat_qubo_to_ising',
+           'probmat_ising_to_qubo',
+           'create_random_initial_states',
+           'calc_ising_energy_from_states'
+           ]
