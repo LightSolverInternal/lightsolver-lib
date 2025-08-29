93c4ed9edacc04d1627cce6484c2ef0f6f888e7b
AvigailLS <avigail@lightsolver.com>
2025-08-29 19:46:20 +0300

    Update __init__.py

--- ls_lib/__init__.py
+++ ls_lib/__init__.py
@@ -3,6 +3,8 @@ from .ls_lib import probmat_qubo_to_ising
 from .ls_lib import probmat_ising_to_qubo
 from .ls_lib import create_random_initial_states
 from .ls_lib import calc_ising_energy_from_states
+from .ls_lib import XYModelParams
+from .ls_lib import coupling_matrix_xy
 
 __all__ = ['calc_ising_energies',
            'probmat_qubo_to_ising',
